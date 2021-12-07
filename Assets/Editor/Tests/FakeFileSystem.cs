e8ab628836c3534b1993c8d9d28a72a3c78c604e
Alexandr_Bros <sogra2005@icloud.com>
2021-12-07 14:50:38 +0700

    Update FakeFileSystem.cs

--- Assets/Editor/Tests/FakeFileSystem.cs
+++ Assets/Editor/Tests/FakeFileSystem.cs
@@ -3,9 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Geo.Data;
+using NUnit.Framework;
 
 namespace Tests
 {
+	class MytestSearch_max
+	{
+		[Test]
+		public void TestSearch_max()
+		{
+			int[] data_1 = new int[]{1, 2, 3, 4, 5};
+			int[] data_2 = new int[]{3, 5, 12, 17, 0};
+			int[] data_3 = new int[]{17, 6, 9};
+			int[] data_4 = new int[]{9, 3, 5, 7, 1, 11, 15};
+			int[] data_5 = new int[]{1, 2, 3, -4, -125, 11, 99, 34, 0, 1};
+			Assert.AreEqual(Max(data_1), 4);
+			Assert.AreEqual(Max(data_2), 3);
+			Assert.AreEqual(Max(data_3), 0);
+			Assert.AreEqual(Max(data_4), 6);
+			Assert.AreEqual(Max(data_5), 6);
+		}
+		
+		int Max(int[] data)
+		{
+			int id = 0, max_size = data[0];
+			for (int i = 1; i < data.Length; i++)
+			{
+				if (data[i] > max_size)
+				{
+					max_size = data[i];
+					id = i;
+				}
+			}
+			return id;
+		}
+	}
 	class FakeFileSystem : IFileSystem
 	{
 		public List<string> paths = new List<string>();
